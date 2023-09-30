33ba74f10800e345a663b6e2a30a567ae7109584
Almy <121775226+kuaks96@users.noreply.github.com>
2023-09-30 02:21:22 -0300

    Create Calculadora.h

---
+++ Calculadora.h
@@ -0,0 +1,14 @@
+// Calculadora.h
+#ifndef CALCULADORA_H
+#define CALCULADORA_H
+
+class Calculadora {
+public:
+  // Funciones para realizar operaciones
+  double sumar(double a, double b);
+  double restar(double a, double b);
+  double multiplicar(double a, double b);
+  double dividir(double a, double b);
+};
+
+#endif 
